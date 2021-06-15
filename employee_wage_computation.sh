3b02c8664a2816a8e8557b017907a077d763e011
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-15 12:22:14 +0530

    add for a month

--- employee_wage_computation.sh
+++ employee_wage_computation.sh
@@ -9,7 +9,18 @@ echo "Employee is Present "
 fi 
 wage_per_hour=20 
 full_day=8
-wage=$(($wage_per_hour * $full_day)) 
-read -p "Add part time hour"part_time_hour
-case $part_time_hour
+part_time_hour=8 
+read -p "Enter what wage you want to calculate 1. full time 2. part time" choice
+	case $choice in
+	1)wage=$(($wage_per_hour * $full_day))
+	echo $wage
+	wage_for_month_full=$(($wage * 20))
+	echo "Wage for a month for full time $wage_for_month_full"
+		;;
+	2)part_time_employee_wage=$(($wage_per_hour * $part_time_hour))
+	echo $part_time_employee_wage
+	wage_for_month_part=$(($part_time_employee_wage * 20))
+	echo "Wage for a month for part time $wage_for_month_part"
+		;;
 esac
+
