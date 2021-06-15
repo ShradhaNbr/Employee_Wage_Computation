f65b4bcd40c572ccda410676d72df18935c32540
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-15 12:46:05 +0530

    adding conditions

--- employee_wage_computation.sh
+++ employee_wage_computation.sh
@@ -9,18 +9,31 @@ echo "Employee is Present "
 fi 
 wage_per_hour=20 
 full_day=8
-part_time_hour=8 
+part_time_hour=8
+total_wrk_day=20
 read -p "Enter what wage you want to calculate 1. full time 2. part time" choice
 	case $choice in
 	1)wage=$(($wage_per_hour * $full_day))
 	echo $wage
-	wage_for_month_full=$(($wage * 20))
+	read -p "enter total working hours" total_wrk_hrs
+	if [ $total_wrk_day -le 20 -a $total_wrk_hrs -le 100 ]
+	then
+	wage_for_month_full=$(($wage * $total_wrk_day))
 	echo "Wage for a month for full time $wage_for_month_full"
+	else
+	exit
+	fi
 		;;
 	2)part_time_employee_wage=$(($wage_per_hour * $part_time_hour))
 	echo $part_time_employee_wage
-	wage_for_month_part=$(($part_time_employee_wage * 20))
+	read -p "enter total working hours" total_wrk_hrs
+	if [ $total_wrk_day -le 20 -a $total_wrk_hrs -le 100 ]
+        then
+	wage_for_month_part=$(($part_time_employee_wage * $total_wrk_day))
 	echo "Wage for a month for part time $wage_for_month_part"
+	else
+	exit
+	fi
 		;;
 esac
 
