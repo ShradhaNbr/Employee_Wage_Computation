e2983ea90e12d449847e472eb9919bc50f249971
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-15 12:08:00 +0530

    add : Using switch

--- employee_wage_computation.sh
+++ employee_wage_computation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Welcome to Wage Computation Program" 
 random_check=$((RANDOM%2)) 
 if [ $random_check -eq 0 ] 
@@ -9,7 +9,14 @@ echo "Employee is Present "
 fi 
 wage_per_hour=20 
 full_day=8
-wage=$(($wage_per_hour * $full_day)) 
-read -p "Calculate Wage"$wage
-case $wage
+part_time_hour=8 
+part_time_employee_wage=$(($wage_per_hour * $part_time_hour))
+read -p "Enter what wage you want to calculate 1. full time 2. part time" choice
+	case $choice in
+	1)wage=$(($wage_per_hour * $full_day))
+	echo $wage 
+		;;
+	2)part_time_employee_wage=$(($wage_per_hour * $part_time_hour))
+	echo $part_time_employee_wage 
+		;;
 esac
