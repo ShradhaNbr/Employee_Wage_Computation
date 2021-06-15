25439d64c7dbaf949b619a5165cfa2c39026c4ab
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-15 10:55:12 +0530

    add : Employee Wage calculation

--- employee_wage_computation.sh
+++ employee_wage_computation.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
-echo "Welcome to Wage Computation Program"
-random_check=$((RANDOM%2))
-if [ $random_check -eq 0 ]
-then
-echo "Employee is absent "
-else
-echo "Employee is Present "
-fi
+echo "Welcome to Wage Computation Program" 
+random_check=$((RANDOM%2)) 
+if [ $random_check -eq 0 ] 
+then 
+echo "Employee is absent " 
+else 
+echo "Employee is Present " 
+fi 
+wage_per_hour=20 
+full_day=8 
+wage=$(($wage_per_hour * $full_day)) 
